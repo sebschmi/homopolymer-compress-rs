49b59d7b4936534f854d34ffd624d72686fb7108
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2022-03-03 10:45:37 +0200

    Fix hoco function.

--- src/lib.rs
+++ src/lib.rs
@@ -10,17 +10,33 @@ pub fn homopolymer_compress<
 >(
     input: Input,
 ) -> impl 'output + Iterator<Item = Item> {
-    input.into_iter().scan(None, |previous_item, item| {
-        if let Some(previous_item) = previous_item.as_mut() {
-            if *previous_item == item {
-                None
+    input
+        .into_iter()
+        .scan(None, |previous_item, item| {
+            if let Some(previous_item) = previous_item.as_mut() {
+                if *previous_item == item {
+                    Some(None)
+                } else {
+                    *previous_item = item.clone();
+                    Some(Some(item))
+                }
             } else {
-                *previous_item = item.clone();
-                Some(item)
+                *previous_item = Some(item.clone());
+                Some(Some(item))
             }
-        } else {
-            *previous_item = Some(item.clone());
-            Some(item)
-        }
-    })
+        })
+        .flatten()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::homopolymer_compress;
+
+    #[test]
+    fn test_homopolymer_compression() {
+        let input = b"ACAARRRTGGGTGTJASAAAI";
+        let expected = Vec::from_iter(b"ACARTGTGTJASAI".iter().cloned());
+        let actual = Vec::from_iter(homopolymer_compress(input.iter().cloned()));
+        assert_eq!(expected, actual);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::fs::File;
-use std::path::PathBuf;
 use bio::io::fasta;
-use log::{error, info, LevelFilter};
-use simplelog::{ColorChoice, TerminalMode, TermLogger};
 use clap::Parser;
 use crossbeam::{channel, thread};
 use homopolymer_compress::homopolymer_compress;
+use log::{info, LevelFilter};
+use simplelog::{ColorChoice, TermLogger, TerminalMode};
+use std::fs::File;
+use std::path::PathBuf;
 
 #[derive(Parser)]
 struct Configuration {
@@ -29,7 +29,13 @@ struct Configuration {
 }
 
 fn initialise_logging() {
-    TermLogger::init(LevelFilter::Debug, Default::default(), TerminalMode::Stderr, ColorChoice::Auto).unwrap();
+    TermLogger::init(
+        LevelFilter::Debug,
+        Default::default(),
+        TerminalMode::Stderr,
+        ColorChoice::Auto,
+    )
+    .unwrap();
     info!("Logging initialised successfully")
 }
 
@@ -37,47 +43,86 @@ fn main() {
     let configuration = Configuration::parse();
     initialise_logging();
 
-    if !configuration.input.ends_with(".fasta") && !configuration.input.ends_with(".fa") {
-        error!("Only fasta files supported at the moment, must end in .fa or .fasta");
+    let input = configuration.input;
+    if let Some(extension) = input.extension() {
+        if extension != "fasta" && extension != "fa" {
+            panic!("Only fasta files supported at the moment, must end in .fa or .fasta, but ends in: {extension:?}");
+        }
+    } else {
+        panic!("Only fasta files supported at the moment, must end in .fa or .fasta, but no extension found: {input:?}");
     }
 
     thread::scope(|scope| {
-        let input_file = File::open(&configuration.input).unwrap_or_else(|error| panic!("Cannot open input file: {error:?}"));
+        let input_file =
+            File::open(&input).unwrap_or_else(|error| panic!("Cannot open input file: {error:?}"));
         let (input_sender, input_receiver) = channel::bounded(configuration.buffer_size);
-        scope.builder().name("input_thread".to_string()).spawn(move |_| {
-            for record in fasta::Reader::new(input_file).records() {
-                let record = record.unwrap_or_else(|error| panic!("Cannot read fasta record: {error:?}"));
-                input_sender.send(record).unwrap_or_else(|error| panic!("Cannot send fasta record: {error:?}"));
-            }
-        }).unwrap_or_else(|error| panic!("Cannot spawn input thread: {error:?}"));
+        scope
+            .builder()
+            .name("input_thread".to_string())
+            .spawn(move |_| {
+                for record in fasta::Reader::new(input_file).records() {
+                    let record = record
+                        .unwrap_or_else(|error| panic!("Cannot read fasta record: {error:?}"));
+                    input_sender
+                        .send(record)
+                        .unwrap_or_else(|error| panic!("Cannot send fasta record: {error:?}"));
+                }
+            })
+            .unwrap_or_else(|error| panic!("Cannot spawn input thread: {error:?}"));
 
-        let (output_sender, output_receiver) = channel::bounded::<(String, Option<String>, Vec<u8>)>(configuration.buffer_size);
+        let (output_sender, output_receiver) =
+            channel::bounded::<(String, Option<String>, Vec<u8>)>(configuration.buffer_size);
         if let Some(output) = configuration.output {
-            let output_file = File::create(&output).unwrap_or_else(|error| panic!("Cannot create output file: {error:?}"));
-            scope.builder().name("output_thread".to_string()).spawn(move |_| {
-                let mut writer = fasta::Writer::new(output_file);
-                while let Ok((id, description, sequence)) = output_receiver.recv() {
-                    writer.write(&id, description.as_ref().map(String::as_str), &sequence).unwrap_or_else(|error| panic!("Cannot write fasta record: {error:?}"));
-                }
-            }).unwrap_or_else(|error| panic!("Cannot spawn output thread: {error:?}"));
+            let output_file = File::create(&output)
+                .unwrap_or_else(|error| panic!("Cannot create output file: {error:?}"));
+            scope
+                .builder()
+                .name("output_thread".to_string())
+                .spawn(move |_| {
+                    let mut writer = fasta::Writer::new(output_file);
+                    while let Ok((id, description, sequence)) = output_receiver.recv() {
+                        writer
+                            .write(&id, description.as_deref(), &sequence)
+                            .unwrap_or_else(|error| panic!("Cannot write fasta record: {error:?}"));
+                    }
+                })
+                .unwrap_or_else(|error| panic!("Cannot spawn output thread: {error:?}"));
         } else {
-            scope.builder().name("output_thread".to_string()).spawn(move |_| {
-                let mut writer = fasta::Writer::new(std::io::stdout());
-                while let Ok((id, description, sequence)) = output_receiver.recv() {
-                    writer.write(&id, description.as_ref().map(String::as_str), &sequence).unwrap_or_else(|error| panic!("Cannot write fasta record: {error:?}"));
-                }
-            }).unwrap_or_else(|error| panic!("Cannot spawn output thread: {error:?}"));
+            scope
+                .builder()
+                .name("output_thread".to_string())
+                .spawn(move |_| {
+                    let mut writer = fasta::Writer::new(std::io::stdout());
+                    while let Ok((id, description, sequence)) = output_receiver.recv() {
+                        writer
+                            .write(&id, description.as_deref(), &sequence)
+                            .unwrap_or_else(|error| panic!("Cannot write fasta record: {error:?}"));
+                    }
+                })
+                .unwrap_or_else(|error| panic!("Cannot spawn output thread: {error:?}"));
         }
 
         for thread_id in 0..configuration.threads {
             let input_receiver = input_receiver.clone();
             let output_sender = output_sender.clone();
-            scope.builder().name(format!("compute_thread_{thread_id}")).spawn(move |_| {
-                while let Ok(record) = input_receiver.recv() {
-                    let hoco_sequence: Vec<u8> = homopolymer_compress(record.seq().iter().cloned()).collect();
-                    output_sender.send((record.id().to_owned(), record.desc().map(str::to_owned), hoco_sequence)).unwrap_or_else(|error| panic!("Cannot send fasta record: {error:?}"));
-                }
-            }).unwrap_or_else(|error| panic!("Cannot spawn compute thread: {error:?}"));
+            scope
+                .builder()
+                .name(format!("compute_thread_{thread_id}"))
+                .spawn(move |_| {
+                    while let Ok(record) = input_receiver.recv() {
+                        let hoco_sequence: Vec<u8> =
+                            homopolymer_compress(record.seq().iter().cloned()).collect();
+                        output_sender
+                            .send((
+                                record.id().to_owned(),
+                                record.desc().map(str::to_owned),
+                                hoco_sequence,
+                            ))
+                            .unwrap_or_else(|error| panic!("Cannot send fasta record: {error:?}"));
+                    }
+                })
+                .unwrap_or_else(|error| panic!("Cannot spawn compute thread: {error:?}"));
         }
-    }).unwrap_or_else(|error| panic!("Error: {error:?}"));
+    })
+    .unwrap_or_else(|error| panic!("Error: {error:?}"));
 }
