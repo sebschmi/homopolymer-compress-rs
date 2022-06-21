d1d3c4ada6670571c68178f65702d96446adae2e
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2022-06-21 07:59:14 +0300

    Compute hodeco mapping when compressing.

--- src/lib.rs
+++ src/lib.rs
@@ -28,9 +28,37 @@ pub fn homopolymer_compress<
         .flatten()
 }
 
+/// Homopolymer compress the given sequence and compute a map to homopolymer decompress the output.
+pub fn homopolymer_compress_with_hodeco_map<
+    'output,
+    Input: 'output + IntoIterator<Item = Item>,
+    Item: 'output + Eq + Clone,
+>(
+    input: Input,
+) -> impl 'output + Iterator<Item = (Item, usize)> {
+    input
+        .into_iter()
+        .enumerate()
+        .scan(None, |previous_item, (index, item)| {
+            if let Some(previous_item) = previous_item.as_mut() {
+                if *previous_item == item {
+                    Some(None)
+                } else {
+                    *previous_item = item.clone();
+                    Some(Some((item, index)))
+                }
+            } else {
+                *previous_item = Some(item.clone());
+                Some(Some((item, index)))
+            }
+        })
+        .flatten()
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::homopolymer_compress;
+    use crate::{homopolymer_compress, homopolymer_compress_with_hodeco_map};
+    use std::iter;
 
     #[test]
     fn test_homopolymer_compression() {
@@ -38,5 +66,22 @@ mod tests {
         let expected = Vec::from_iter(b"ACARTGTGTJASAI".iter().cloned());
         let actual = Vec::from_iter(homopolymer_compress(input.iter().cloned()));
         assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_hodeco_mapping() {
+        let input = b"ACAARRRTGGGTGTJASAAAI";
+        let expected = Vec::from_iter(b"ACARTGTGTJASAI".iter().cloned());
+        let (actual, mut hodeco_mapping): (Vec<_>, Vec<_>) =
+            homopolymer_compress_with_hodeco_map(input.iter().cloned()).unzip();
+        hodeco_mapping.push(input.len());
+        let hodeco_mapping = hodeco_mapping;
+        assert_eq!(expected, actual);
+        let hodeco: Vec<_> = actual
+            .into_iter()
+            .zip(hodeco_mapping.windows(2))
+            .flat_map(|(item, count)| iter::repeat(item).take(count[1] - count[0]))
+            .collect();
+        assert_eq!(hodeco, input);
     }
 }
